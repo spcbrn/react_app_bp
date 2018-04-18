efdf1350e3b84b36952fe1a01c9eaf836e81d852
Christopher Lemke <spcbrn1@gmail.com>
2018-04-18 13:55:38 -0600

    updating the d0m!

--- src/DOM/App.js
+++ src/DOM/App.js
@@ -3,17 +3,27 @@ import React, { Component } from 'react';
 class App extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            list: []
+        }
     }
 
     testMethod = () => {
-        console.log('This is test...');
+        this.setState((ps, p) => ({ list: [...ps.list, 'Another one.'] }))
     }
 
     render() {
+        const renderList = () => {
+            return this.state.list.length
+                ? this.state.list.map((c, i) => <p>{c}</p>)
+                : null;
+        }
+
         return (
             <main>
                 <h1>Hello world...</h1>
                 <button onClick={this.testMethod}>Test</button>
+                { renderList() }
             </main>
         )
     }
